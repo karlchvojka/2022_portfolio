4659ef994a07ce669edd002681543d5a38b34380
Karl Chvojka <karl.chvojka@protonmail.com>
2022-08-16 20:27:40 -0400

    DAT: Imp skills dataset

---
+++ src/globalAssets/data/skills.js
@@ -0,0 +1,19 @@
+const skills = [
+  {
+    key: 'lang',
+    def: true,
+    title: 'Languages'
+  },
+  {
+    key: 'libAndFrame',
+    def: false,
+    title: 'Libraries and Frameworks'
+  },
+  {
+    key: 'dbSys',
+    def: false,
+    title: 'Database Systems'
+  },
+]
+
+export default skills
