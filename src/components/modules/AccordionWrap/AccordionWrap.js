eece91dd22c35d0394cb9af9ca17e24b503b4e27
Karl Chvojka <karl.chvojka@protonmail.com>
2022-08-16 20:28:46 -0400

    IMP: Accordion Wrap

---
+++ src/components/modules/AccordionWrap/AccordionWrap.js
@@ -0,0 +1,30 @@
+// Framework Imports
+import React from 'react'
+
+// Component Imports
+import AccordionItem from 'modules/AccordionItem/AccordionItem'
+
+// CSS Imports
+import StyledAccordionWrap from './StyledAccordionWrap'
+
+/**
+ * This the AccordionWrap component.
+ * @param { array } data - Array of objects.
+ */
+const AccordionWrap = ({ data }) => {
+  return (
+    <StyledAccordionWrap>
+      { 
+        data.map(( section, index ) => (
+          <AccordionItem 
+            key={section.key}
+            def={section.def}
+            title={section.title}
+          />
+        ))
+      }
+    </StyledAccordionWrap>
+  )
+}
+
+export default AccordionWrap
