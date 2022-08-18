5c8776467ba406db324b69f3e70389b6ba700244
Karl Chvojka <karl.chvojka@protonmail.com>
2022-08-18 17:03:36 -0400

    REF: Abstract out the homepage.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 // Framework Imports
 import React from 'react'
 
+// Library Imports
+import { Routes, Route, Link } from "react-router-dom";
+
 // Component Imports
-import Header from 'layouts/Header/Header'
-import HeroImage from 'layouts/HeroImage/HeroImage'
-import Skills from 'layouts/Skills/Skills'
-import Projects from 'layouts/Projects/Projects'
+import Home from 'pages/Home/Home'
 
 // CSS Imports
 import GlobalStyle from 'src/globalConfig/GlobalStyles'
@@ -17,10 +17,7 @@ const App = () => {
     <StyledApp>
       <GlobalFonts />
       <GlobalStyle />
-      <Header />
-      <HeroImage />
-      <Projects />
-      <Skills />
+      <Home/>
     </StyledApp>
   )
 }
