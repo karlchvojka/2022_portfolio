8524e465a84d11186c10ddab9eba4a74345895e2
Karl Chvojka <karl.chvojka@protonmail.com>
2022-08-18 12:40:40 -0400

    REF: Cleaning up file tree

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 // Framework Imports
 import React from 'react'
 
-// Library Imports
-import Header from 'modules/Header/Header'
-
 // Component Imports
-import GlobalStyle from 'src/globalConfig/GlobalStyles'
-import HeroImage from 'modules/HeroImage/HeroImage'
+import Header from 'layouts/Header/Header'
+import HeroImage from 'layouts/HeroImage/HeroImage'
 import Skills from 'layouts/Skills/Skills'
 
 // CSS Imports
+import GlobalStyle from 'src/globalConfig/GlobalStyles'
 import GlobalFonts from 'src/globalAssets/fonts/fonts'
 import StyledApp from './StyledApp'
 
