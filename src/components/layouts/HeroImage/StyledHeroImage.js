d431a2aed9848150aeaf7c1a3d8af4879681b283
Karl Chvojka <karl.chvojka@protonmail.com>
2022-08-20 19:00:38 -0400

    STY: Hero Responsive fixes

--- src/components/layouts/HeroImage/StyledHeroImage.js
+++ src/components/layouts/HeroImage/StyledHeroImage.js
@@ -1,120 +1,88 @@
 // Library Imports
 import styled from 'styled-components'
+import media from 'globalConfig/mobile'
 
 // Asset Imports
 import karlSlider from './assets/karlSlider.jpg'
 
 const StyledHeroImage = styled.section`
-  background-image:url(${karlSlider});
-  background-position:center;
-  background-repeat:no-repeat;
-  background-size:cover;
+  /* MOBILE FIRST DEFAULT STYLES */
+
+  background-image: url(${karlSlider});
+  background-position: left;
+  background-repeat: no-repeat;
+  background-size: cover;
   -webkit-box-shadow: 0px 7px 5px -1px rgba(0,171,223,0.58); 
   box-shadow: 0px 7px 5px -1px rgba(0,171,223,0.58);
-  display:flex;
-  height:300px;
-  margin:0 auto;
+  display: flex;
+  height: 300px;
+  margin: 0 auto;
+  width: 100%;
 
   div {
     align-self: center;
-    margin: 0 30px;
-    width: 50%;
+    margin: 0 auto;
+    width: 95%;
 
     h1 {
       color: #00abdf;
       display: block;
-      font-size: 25px;
-      text-shadow: 3px 3px 2px #000;
       text-transform: uppercase;
     }
 
     p {
-      color:#fff;
-      font-size: 12px;
-      line-height: 16px;
+      color: #fff;
     }
   }
 
-  @media (min-width: 576px) {
-    height:300px;
+  /* Min width of 375 */
+  ${media.iphoneSe `
+    div {
+      max-width: 90%;
+      margin: 0px auto;
+    }
+  `}
+
+  /* Min width of 414 */
+  ${media.iphoneXr `
+    div {
+      max-width: 65%;
+      margin: 0px 0px 0px 20px;
+    }
+  `}
+
+  /* Min width of 576 */
+  ${media.landscapePhones `
+    div {
+      min-width: 50%;
+      max-width: 55%;
+      margin: 0px 0px 0px 20px;
+    }
+  `}
+
+  /* Min width of 768 */
+  ${media.tablet `
+    background-position: middle;
+    height: 400px;
 
     div {
       width: 50%;
-
-      h1 {
-        font-size: 20px;
-      }
-
-      p {
-        font-size: 14px;
-        line-height: 18px;
-      }
     }
-  }
-  @media (min-width: 768px) {
-    height:350px;
+  `}
+
+/* Min width of 1024 */
+  ${media.desktop `
+    height: 480px;
 
     div {
-      width: 50%;
-
-      h1 {
-        font-size: 25px;
-      }
-
-      p {
-        font-size: 16px;
-        line-height: 20px;
-      }
+      width: 55%;
     }
-  }
-  @media (min-width: 992px) {
-    height:400px;
+  `}
 
-    div {
-      width: 50%;
-
-      h1 {
-        font-size: 30px;
-      }
-
-      p {
-        font-size: 18px;
-        line-height: 22px;
-      }
-    }
-  }
-  @media (min-width: 1200px) {
-    height:600px;
-
-    div {
-      width: 50%;
-
-      h1 {
-        font-size: 30px;
-      }
-
-      p {
-        font-size: 18px;
-        line-height: 22px;
-      }
-    }
-  }
-  @media (min-width: 1400px) {
-    height:600px;
-
-    div {
-      width: 50%;
-
-      h1 {
-        font-size: 30px;
-      }
-
-      p {
-        font-size: 18px;
-        line-height: 22px;
-      }
-    }
-  }
+/* Min width of 1200 */
+  ${media.widescreen `
+    height: 600px;
+  `}
 `
 
 export default StyledHeroImage
